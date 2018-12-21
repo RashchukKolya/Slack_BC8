8c6e601022977d4548aae6ba494a93421777dce5
Nick <raschyk1993@gmail.com>
2018-12-21 13:13:41 +0200

    appMarkup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import SidePanel from './SidePanel/SidePanel';
+import Messeges from './Messeges/Messeges';
+import ColorPanel from './ColorPanel/ColorPanel';
+import MetaPanel from './MetaPanel/MetaPanel';
+import { Grid } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      App
-      </div>
+      <Grid columns='equal' className='app'>
+      <ColorPanel/>
+      <SidePanel/>
+      <Grid.Column textAlign='center'>
+        <Messeges/>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel/>
+      </Grid.Column>
+      </Grid>
     );
   }
 }
---
+++ src/Messeges/Messeges.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Messeges extends Component {
+  render() {
+    return (
+      <div>
+        Messeges
+      </div>
+    );
+  }
+}
+
+export default Messeges;

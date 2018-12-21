ee688fedd9093383205ed2496ca5b17aeb9e85b8
Nick <raschyk1993@gmail.com>
2018-12-21 12:56:20 +0200

    spinner

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -6,6 +6,7 @@ import firebase from '../firebase';
 import App from '../App';
 import Login from '../Auth/Login';
 import Register from '../Auth/Register';
+import Spiner from '../Spiner/Spiner.jsx';
 
 class Root extends Component {
   componentDidMount(){
@@ -18,18 +19,21 @@ class Root extends Component {
     })
   }
   render() {
-    return (
-      <div>
+    return this.props.isLoading ? <Spiner/> :(
+
         <Switch>
           <Route exact path="/" component={App}/>
           <Route path="/login" component={Login}/>
           <Route path="/registration" component={Register}/>
         </Switch>
-      </div>
     );
   }
 }
-
+function MSTP(state) {
+  return {
+    isLoading: state.user.isLoading,
+  }
+}
 function MDTP(dispatch){
   return {
     setUser: function(user){
@@ -37,4 +41,4 @@ function MDTP(dispatch){
     }
   }
 } 
-export default  withRouter(connect(null,MDTP)(Root));
+export default  withRouter(connect(MSTP,MDTP)(Root));

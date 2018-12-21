ae97b038a3682f216b842f670cde3c465d3d519b
Nick <raschyk1993@gmail.com>
2018-12-21 12:09:37 +0200

    redux

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Switch, Route, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {setUser} from '../redux/action/setUserAction.js';
 import firebase from '../firebase';
 import App from '../App';
 import Login from '../Auth/Login';
@@ -10,6 +12,7 @@ class Root extends Component {
     firebase.auth().onAuthStateChanged(user => {
       if(user) {
         console.log(user);
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     })
@@ -27,4 +30,11 @@ class Root extends Component {
   }
 }
 
-export default withRouter(Root);
+function MDTP(dispatch){
+  return {
+    setUser: function(user){
+      dispatch(setUser(user))
+    }
+  }
+} 
+export default  withRouter(connect(null,MDTP)(Root));
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import Root from './Root/Root'
 import 'semantic-ui-css/semantic.min.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+<Provider store={store}>
 <BrowserRouter>
 <Root/>
-</BrowserRouter>, document.getElementById('root'));
+</BrowserRouter>
+</Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

07d8b1cd8670c5a1bed32f507b022494f6535853
Nick <raschyk1993@gmail.com>
2018-12-21 11:10:04 +0200

    redirect

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, withRouter} from 'react-router-dom';
+import firebase from '../firebase';
 import App from '../App';
 import Login from '../Auth/Login';
 import Register from '../Auth/Register';
 
 class Root extends Component {
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        console.log(user);
+        this.props.history.push('/');
+      }
+    })
+  }
   render() {
     return (
       <div>
@@ -18,4 +27,4 @@ class Root extends Component {
   }
 }
 
-export default Root;
+export default withRouter(Root);

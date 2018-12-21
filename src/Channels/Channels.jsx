bcb45639e0c0b8cfd6444229f8e5427f606c5ca7
Nick <raschyk1993@gmail.com>
2018-12-21 17:00:11 +0200

    modalChannel

--- src/Channels/Channels.jsx
+++ src/Channels/Channels.jsx
@@ -1,22 +1,68 @@
 import React, { Component } from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon, Modal, Form, Button } from 'semantic-ui-react';
 
 class Channels extends Component {
 
   state = {
-    channels: []
+    channels: [],
+    name: '',
+    descript: '',
+    modal: false,
   }
   
+  showModal = () => {
+    this.setState({
+      modal: true
+    })
+  }
+  
+  closeModal = () => {
+    this.setState({
+      modal: false
+    })
+  }
+  handlerChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  addChanel = () => {
+    this.setState(prev => ({
+      channels: [...prev.channels, {channel: this.state.name, chanel: this.state.descript}],
+      modal: false,
+    }))
+        
+  }
   render() {
-    const {channels} = this.state;
+    const {channels, modal} = this.state;
     return (
+      <React.Fragment>
       <Menu.Menu style={{paddingBottom: '2rem'}}>
         <Menu.Item>
           <span>
             <Icon name='exchange'/>CHANNELS
-          </span> ({channels.length})<Icon name='add'/>
+          </span>({channels.length})<Icon name='add' onClick={this.showModal}/>
         </Menu.Item>
       </Menu.Menu>
+      <Modal open={modal}>
+        <Modal.Header>
+          Add channel
+        </Modal.Header>
+        <Modal.Content>
+          <Form.Input fluid name='name' placeholder='Enter name of chanel' type='text' onChange={this.handlerChange}/>
+          <Form.Input fluid name='descript' placeholder='Enter description' type='text' onChange={this.handlerChange}/>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color='red' inverted onClick={this.closeModal}>
+            Cancel
+          </Button>
+          <Button  color='green' inverted onClick={this.addChanel}>
+            Add
+          </Button>
+        </Modal.Actions>
+      </Modal>
+      </React.Fragment>
     );
   }
 }

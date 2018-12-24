a15bc21e4e618782b6a6d781c2b5fb904e13d4ad
Nick <raschyk1993@gmail.com>
2018-12-24 18:02:26 +0200

    addMessagesToScreen

--- src/Messeges/Messeges.jsx
+++ src/Messeges/Messeges.jsx
@@ -3,20 +3,44 @@ import MessageHeader from '../MessageHeader/MessageHeader';
 import { Segment, Comment } from 'semantic-ui-react';
 import MessageForm from '../MessageForm/MessageForm';
 import firebase from '../firebase';
+import {connect} from 'react-redux'
+import SingleMessage from '../SingleMessage/SingleMessage';
 
 
 class Messeges extends Component {
   state = {
-    messagesRef: firebase.database().ref('messages')
+    messagesRef: firebase.database().ref('messages'),
+    messages: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    setTimeout(()=>{
+      const {currentChannel, currentUser} = this.props;
+      if(currentChannel && currentUser) {
+        this.addListeners(currentChannel.id)
+      }
+    }, 1000)
+  }
+  addListeners = channelId => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on('child_added', snap => {
+      loadedMessages.push(snap.val())
+      this.setState({
+        messages: loadedMessages,
+        loading: false
+      })
+    })
   }
   render() {
-    const {messagesRef} = this.state;
+    const {messagesRef, messages} = this.state;
     return (
       <React.Fragment>
         <MessageHeader/>
         <Segment>
           <Comment.Group
           className='message'>
+            {messages.length > 0 && messages.map(message => <SingleMessage key={message.time} message={message} user={message.user}/>)}
           </Comment.Group>
         </Segment>
         <MessageForm messagesRef={messagesRef}/>
@@ -24,5 +48,10 @@ class Messeges extends Component {
     );
   }
 }
-
-export default Messeges;
+function mapStateToProps(state){
+  return {
+    currentChannel: state.channel,
+    currentUser: state.user.currentUser,
+  }
+}
+export default connect(mapStateToProps, null)(Messeges);
